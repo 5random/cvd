c08a75be8c41e18e342317006122fc01dce1001a
5random <94985737+5random@users.noreply.github.com>
2025-06-07 15:22:29 +0200

    Add required dependencies to setup.py for project functionality

--- setup.py
+++ setup.py
@@ -3,6 +3,18 @@ from setuptools import setup, find_packages
 setup(
     name='cvd',
     version='0.1.0',
+    install_requires=[
+        'plotly',
+        'nicegui',
+        'numpy',
+        'pyserial',
+        'opencv-python',
+        'Pillow',
+        'pytest',
+        'pytest-asyncio',
+        'watchdog',
+        'jsonschema',
+    ],
     packages=find_packages('program'),
     package_dir={'': 'program'},
 )
