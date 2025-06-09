2f909fba9cfaf442e37a2102e878e76c587cb6cc
5random <94985737+5random@users.noreply.github.com>
2025-06-09 18:48:28 +0200

    Use dynamic delay in capture loop

--- program/src/controllers/algorithms/motion_detection.py
+++ program/src/controllers/algorithms/motion_detection.py
@@ -648,4 +648,4 @@ class MotionDetectionController(ImageController):
                     error=str(e),
                 )
 
-            await asyncio.sleep(base_delay)
+            await asyncio.sleep(delay)
