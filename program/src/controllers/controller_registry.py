64e945bd13d2367e40ca28ff5bc3588a7dababa3
5random <94985737+5random@users.noreply.github.com>
2025-06-10 06:45:55 +0200

    Use log service logger in controller manager tests

--- program/src/controllers/controller_registry.py
+++ program/src/controllers/controller_registry.py
@@ -1,14 +1,26 @@
+"""Registry for controller types."""
+
 from typing import Dict, Type
 
 from .controller_base import ControllerStage
 from .algorithms.motion_detection import MotionDetectionController
 from .algorithms.reactor_state import ReactorStateController
-from .controller_utils.controller_data_sources.camera_capture_controller import CameraCaptureController
+from .controller_utils.controller_data_sources.camera_capture_controller import (
+    CameraCaptureController,
+)
 
 # Mapping of controller type strings to their implementing classes
-CONTROLLER_CLASS_MAP: Dict[str, Type[ControllerStage]] = {
-    "motion_detection": MotionDetectionController,
-    "reactor_state": ReactorStateController,
-    "camera_capture": CameraCaptureController,
-}
+CONTROLLER_CLASS_MAP: Dict[str, Type[ControllerStage]] = {}
+
+
+def register_controller_type(name: str, cls: Type[ControllerStage]) -> None:
+    """Register a controller class under a type name."""
+    CONTROLLER_CLASS_MAP[name] = cls
+
+
+# Register built-in controller types
+register_controller_type("motion_detection", MotionDetectionController)
+register_controller_type("reactor_state", ReactorStateController)
+register_controller_type("camera_capture", CameraCaptureController)
+register_controller_type("camera", CameraCaptureController)
 
