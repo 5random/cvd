c3388be649ff05a305cd25dead8751f55b4fcecf
5random <94985737+5random@users.noreply.github.com>
2025-06-19 17:47:12 +0200

    feat: add alternative GUI application for basic functionality

---
+++ program/src/gui/alt_application.py
@@ -0,0 +1,5 @@
+#alternative gui application for the program that enables only basic functionality of the program those are:
+# - live view of webcam with basic controls (start/stop webcam, adjust ROI, sensitivity settings, fps settings, resolution settings)
+# - live status of motion detection algorithm applied to the webcam with ROI and sensitivity settings
+# - email alert service for critical events (e.g. when motion is not detected) with alert delay settings (email alert shall include webcam image, motion detection status, timestamp, and other relevant information)
+# - basic experiment management (start/stop experiment, view results/status, alert on critical events)
